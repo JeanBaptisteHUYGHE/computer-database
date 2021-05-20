3b5da82d69c95beddee0228a0f76080f6989807c
Jean-Baptiste HUYGHE <jeanbaptiste.huyghe@laposte.net>
2021-05-20 12:22:37 +0200

    Add hashCode() & equals() in model package

--- src/main/java/com/excilys/cdb/model/Computer.java
+++ src/main/java/com/excilys/cdb/model/Computer.java
@@ -92,6 +92,75 @@ public class Computer {
 				this.id, this.name, this.introductionDate, this.discontinueDate, this.manufacturer);
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((discontinueDate == null) ? 0 : discontinueDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((introductionDate == null) ? 0 : introductionDate.hashCode());
+		result = prime * result + ((manufacturer == null) ? 0 : manufacturer.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		Computer other = (Computer) obj;
+		if (discontinueDate == null) {
+			if (other.discontinueDate != null) {
+				return false;
+			}
+		} else {
+			if (!discontinueDate.equals(other.discontinueDate)) {
+				return false;
+			}
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		if (introductionDate == null) {
+			if (other.introductionDate != null) {
+				return false;
+			}
+		} else {
+			if (!introductionDate.equals(other.introductionDate)) {
+				return false;
+			}
+		}
+		if (manufacturer == null) {
+			if (other.manufacturer != null) {
+				return false;
+			}
+		} else {
+			if (!manufacturer.equals(other.manufacturer)) {
+				return false;
+			}
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else {
+			if (!name.equals(other.name)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static class ComputerBuilder {
 		private Integer id = null;
 		private String name;
